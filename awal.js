8b7e20af4a57267eb4d5c133c9f13c7930956bba
yogbay20 <yogabayu834@gmail.com>
2024-12-02 15:04:45 +0700

    first

---
+++ awal.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+      window.location.href = "home.html"; // Ganti dengan halaman utama setelah animasi selesai
+    }, 4000); // Redirect setelah 4 detik
+  });
